01d8f9601ae91e1de599cc1b8c11f859fcf4abaf
on <on@10.218.71.10>
2015-08-14 17:56:16 +0800

    edit expired & inGroup

--- src/java/pdf/AppoinmentLetter.java
+++ src/java/pdf/AppoinmentLetter.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TFMSystem;
-
+package pdf;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,13 +12,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import package1.DB;
+import pdf.letter;
 
 /**
  *
  * @author on
  */
-@WebServlet(name = "changeCoordinator", urlPatterns = {"/changeCoordinator"})
-public class changeCoordinator extends HttpServlet {
+@WebServlet(name = "AppoinmentLetter", urlPatterns = {"/AppoinmentLetter"})
+public class AppoinmentLetter extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -30,47 +30,71 @@ public class changeCoordinator extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    //String noRujukan=null;
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String name=null;
+        String position=null;
+        String department=null;
+        String gStatus=null;
+        String startDate=null;
+        String endDate=null;
+        String taskName=null;
+        String noRujukan;
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            String newCoordinatorID=request.getParameter("newCoordinatorID");
-            String taskID=request.getParameter("taskID");
-            String memberIDoldcoor=request.getParameter("memberIDcoor");
+            String memberID=request.getParameter("memberID");
+            noRujukan=request.getParameter("noRujukan");
             
             DB db = new DB();
+            letter Letter= new letter();
             if(db.connect())
             {
-                if(db.query("UPDATE tf SET coordinatorID='"+newCoordinatorID+"' WHERE idTF='"+taskID+"'"))
+                if(db.query("SELECT user.name, user.position, user.department, tf_member.GStatus, tf_member.StartDate, tf_member.EndDate, tf.TFname FROM user INNER JOIN tf_member ON user.userID=tf_member.userID INNER JOIN tf ON tf_member.tfID=tf.idTF WHERE tf_member.idtf_member='"+memberID+"'"))
                 {
-                    if(db.query("UPDATE tf_member SET GStatus='Coordinator', position='Leader' WHERE userID='"+newCoordinatorID+"' AND tfID='"+taskID+"'"))
-                    {
-                        db.query("SELECT * FROM tf_member WHERE idtf_member='"+memberIDoldcoor+"'");
-                        String oldCoordinatorID=db.getDataAt(0, "userID");
-                        System.out.println("GStatus:"+db.getDataAt(0,"Gstatus")+"\nuserID:"+db.getDataAt(0, "userID"));
-                        if((memberIDoldcoor!=null||!memberIDoldcoor.equals(""))&&(!oldCoordinatorID.equals(newCoordinatorID)))
-                        {
-                            response.sendRedirect("terminateMember?taskID="+taskID+"&memberID="+memberIDoldcoor+"");
-                        }
-                        else
-                            response.sendRedirect("Admin/viewCT.jsp?taskID="+taskID+"");
-                    }
+                    name=db.getDataAt(0, "name");
+                    position=db.getDataAt(0, "position");
+                    department=db.getDataAt(0, "department");
+                    gStatus=db.getDataAt(0, "GStatus");
+                    startDate=db.getDataAt(0, "StartDate");
+                    endDate=db.getDataAt(0, "EndDate");
+                    taskName=db.getDataAt(0, "TFname");
                 }
                 db.close();
             }
-             
+            
+            System.out.println(name);
+                System.out.println(position);
+                System.out.println(department);
+                System.out.println(gStatus);
+                System.out.println(startDate);
+                System.out.println(endDate);
+                System.out.println(taskName);
+                System.out.println(noRujukan);
+            
+            if(Letter.AlterLetter(noRujukan, name, position, department, gStatus, startDate, endDate, taskName))
+            {
+                out.println("Success");
+            }
+            
+            
+            
             
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet changeCoordinator</title>");            
+            out.println("<title>Servlet AppoinmentLetter</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet changeCoordinator at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet AppoinmentLetter at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
+        
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
